e7bc879a1585b541c028e831927155198a989f4e
Adam Fudge <coolfudge@gmail.com>
2017-11-14 23:40:23 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,8 @@
 class CreateArtists < ActiveRecord::Migration
   def change
-    create_tabel :artists do |t|
-      t.string :name
-    end
+    # create_tabel :artists do |t|
+    #   t.string :name
+    #   t.interger :
+    # end
   end
 end
