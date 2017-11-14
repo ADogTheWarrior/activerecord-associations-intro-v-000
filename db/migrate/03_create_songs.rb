6cba420544feb8cdd7c60f865f90459616a23495
Adam Fudge <coolfudge@gmail.com>
2017-11-14 23:40:52 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_tabel :songs do |t|
+      t.string :name
+      t.interger :artist_id
+      t.interger :genre_id
+    end
+  end
 end
