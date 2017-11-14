46064089543633c093a06059e1392d1125a776d6
Adam Fudge <coolfudge@gmail.com>
2017-11-14 23:42:42 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,9 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :name
-      t.interger :artist_id
-      t.interger :genre_id
+      t.string :name 
+      t.integer :artist_id
+      t.integer :genre_id
     end
   end
 end
